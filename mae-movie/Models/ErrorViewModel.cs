9de4f536a9365b11e6e1f461889459cd4d451fcf
soy ale <aecheverria@live.com.ar>
2026-08-04 17:17:03 -0300

    commit inicial

---
+++ mae-movie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mae_movie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
