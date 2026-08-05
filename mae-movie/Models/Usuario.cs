5386b75c2b2e29dbc961804c7a109df5db6f518e
soy ale <aecheverria@live.com.ar>
2026-08-05 10:16:06 -0300

    Armado de Modelos

---
+++ mae-movie/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace mae_movie.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string ImagenPerfilUrl { get; set; }
+        public List<Favorito> ListaFavorito { get; set; }
+
+
+
+    }
+}
